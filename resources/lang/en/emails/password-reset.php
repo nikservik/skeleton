9996f9bd0b5cee9e1446f4cb7dfac4fdb092e35d
Sergey Nikiforov <ser.nikiforov@gmail.com>
2020-04-10 16:00:41 +0200

    email localization en

---
+++ resources/lang/en/emails/password-reset.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'preheader' => 'Password reset link.',
+    'hello' => '<b>Hello, :name!</b>',
+    'line1' => 'We sent this mail to you because you or somebody on behalf of you asked for password reset for «:app».',
+    'line2' => 'If you still want to reset password press button below and set new password.',
+    'button' => 'Reset password',
+    'line3' => 'If you didn\'t asked for password reset – just ignore this email. Nobody can change your password.',
+    'signature' => '<i>Best regards,<br>Yours :name</i>',
+];
---
+++ resources/lang/en/emails/verify-email.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'preheader' => 'Verify your email address to use «:app».',
+    'hello' => '<b>Hello, :name!</b>',
+    'line1' => 'We sent this email to you as second step of registration in «:app».',
+    'line2' => 'To start using it please press button below to verify your email:',
+    'button' => 'Verify my email',
+    'line3' => 'If you did not tried to register in our app – just ignore this email.',
+    'signature' => '<i>Best regards,<br>Yours :name</i>',
+];
